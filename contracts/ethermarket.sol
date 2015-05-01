b8ccbfbfb044c3353bb2635deee1b5d3bc16432d
Iuri Matias <iuri.matias@gmail.com>
2015-04-30 21:40:12 -0400

    update string32 to bytes32; event on product created

--- contracts/ethermarket.sol
+++ contracts/ethermarket.sol
@@ -1,30 +1,32 @@
 contract EtherMarket {
   address owner;
   struct Product {
-    string32 title;
-    string32 description;
+    bytes32 title;
+    bytes32 description;
     uint price;
   }
-  mapping (int64 => Product) products;
+  mapping (int64 => Product) public products;
   int64 productNum;
+
+  event newProductAdded(address indexed creator, int64 productId);
 
   function EtherMarket() {
     owner = msg.sender;
   }
 
-  function addProduct(string32 title, string32 description, uint price) {
+  function addProduct(bytes32 title, bytes32 description, uint price) {
     productNum +=1 ;
     Product product = products[productNum];
     product.title = title;
     product.description = description;
     product.price = price;
+    newProductAdded(msg.sender, productNum);
   }
 
-  function getProduct(int64 id) constant returns (string32 a, string32 b, uint c) {
+  function getProduct(int64 id) constant returns (bytes32 a, bytes32 b, uint c) {
     Product product = products[id];
     a = product.title;
     b = product.description;
     c = product.price;
   }
-
 }
